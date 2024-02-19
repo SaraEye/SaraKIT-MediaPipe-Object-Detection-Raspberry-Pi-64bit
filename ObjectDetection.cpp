7d9e4eafc949d945dabed8415929b724b63f19bb
Artur Majtczak <artur@majtczak.com>
2024-02-20 00:41:53 +0100

    code cleanup

--- ObjectDetection.cpp
+++ ObjectDetection.cpp
@@ -11,28 +11,33 @@
 
 using namespace std;
  
-cv::Mat frame, frameGray, frameGrayHalf, frameGrayHalfEdge;     // cam0
+cv::Mat frame0, frame0Gray, frame0GrayHalf, frame0GrayHalfEdge; // cam0
 cv::Mat frame1, frame1Gray, frame1GrayHalf, frame1GrayHalfEdge; // cam1
-cv::Mat frameBig, imgPreview, imgPreview1, imgWindow, imgcopy0, imgcopy1;
-cv::Mat imgORG, imgEdge;
+cv::Mat imgProcessed;
 
 ViewerStatus viewStatus;
 
 //ctrl-c 
 void ctrlc_handler(sig_atomic_t s){
     printf("\nCaught signal %d\n",s);
-    control_c=true;	
+    control_c=true;
     exit(1);
 }
 
 int main(int argc, char** argv){
     signal(SIGINT,ctrlc_handler);
-    imgEdge=cv::Mat(camheight, camwidth, CV_8UC3);
+	
+	camwidth=640;
+	camheight=480;
 
-    init_camera(0, camwidth, camheight, false, camOpenCV, cambuffered, true, true);
-    //init_camera(1, cam1width, cam1height, false, camOpenCV, cambuffered, false, false);
+    imgProcessed=cv::Mat(camheight, camwidth, CV_8UC3);
 
-    init_viewer();
+    init_camera(0, camwidth, camheight, false, false, true, true, true);
+    //init_camera(1, camwidth, camheight, false, false, true, true, true);
+    sleepms(200);
+
+    //init_viewer(ViewMode::Camera0, ViewMode::Camera1, 1, false, true);
+    init_viewer(ViewMode::Camera0,ViewMode::Processed);
 
     std::vector<mpobject::Object> objects;
 
@@ -41,24 +46,25 @@ int main(int argc, char** argv){
 
     printf("Start Loop\n");
     do {
-        // Get frame to frame,frameGray,frameGrayHalf (CamWide is only gray)dupa
-        if (GetFrame(0)!=1) { //GetFrame()==1 (new frame from cam0, ==2 from cam1, ==3 from cam0 & cam 1)
+        // Get frame to frame,frameGray,frameGrayHalf
+        if (GetFrame()==0) { //GetFrame()==1 (new frame from cam0 ==1, ==2 from cam1, ==3 from cam0 & cam 1)
 
             //here you have time to do anything
+
             sleepms(1);
             continue;
         }
 
         objects.clear();
-        objectSync.pushFrame(mo.pushFrame(frame),frame);
-        int reso=objectSync.getFrameFromId(mo.getObjects(objects),imgEdge);
+        objectSync.pushFrame(mo.pushFrame(frame0),frame0);
+        int reso=objectSync.getFrameFromId(mo.getObjects(objects),imgProcessed);
 
         if (reso&&objects.size()) {
-            mo.drawObjectsRects(imgEdge,objects);
+            mo.drawObjectsRects(imgProcessed,objects);
             tick2on=true;
         }
-        viewStatus = viewer_refresh();
 
+        viewStatus = viewer_refresh();
     } while (viewStatus != ViewerStatus::Exit && control_c != true);
     closing_function(0);
     return 1;
